2546a603022c6dd55c01c6527a17bc19a3ae5d1b
Simin Liu <sliu03@rockefeller.edu>
2021-12-17 18:36:12 -0500

    Fixed some bugs

--- formating.py
+++ formating.py
@@ -8,6 +8,7 @@ def encode_output(container, sequence):
 
 
 def convert_input(rna_true_structure):
+    # This is not correct for getting base pairs: just keep for a record
     first_pair_element = []
     second_pair_element = []
     for i in range(len(rna_true_structure)):
@@ -28,6 +29,8 @@ def convert_input2(rna_true_structure):
         elif rna_true_structure[i] == "(":
             save_element.append(i)
         else:
+            # When you firstly meet a ")", pair it with the closest unpaired "(" on the left side of
+            # the current ")". Then remove this "(" from the list of the unpaired "(".
             pair_index.append((save_element[len(save_element)-1], i))
             save_element.pop()
     return pair_index
